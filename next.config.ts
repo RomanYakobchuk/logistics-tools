5f38c9ff8ee56fe51d1f862ce42205d22a1150ef
RomanYakobchuk <romanyakobchuk28@gmail.com>
2025-02-03 16:05:38 +0200

    drive-distance

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-    output: "standalone",
-    images: {
-        unoptimized: true
-    },
-    experimental: {
-
-    }
 };
 
 export default nextConfig;
